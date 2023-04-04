928a6564f1c4a22bd062d32c12159fafaeb4ad07
Stepan Khamaza <khamaza04@mail.ru>
2023-04-04 11:41:09 +0500

    added changes in Book

--- src/main/java/ru/urfu/Book.java
+++ src/main/java/ru/urfu/Book.java
@@ -7,7 +7,8 @@ public class Book {
     private String author;
     private Integer year;
 
-    public Book() {}
+    public Book() {
+    }
 
     public Book(String name, String author, Integer year) {
         this.name = name;
@@ -32,23 +33,11 @@ public class Book {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
     public Integer getYear() {
         return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
     }
 }
